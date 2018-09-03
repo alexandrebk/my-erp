b7a3dda44e4e1a346a5ceb2f4ab374eee9235a05
alexandrebk <alexandre.b2506@gmail.com>
2018-09-03 09:22:58 +0200

    index true

--- db/migrate/20180622075331_create_tasks.rb
+++ db/migrate/20180622075331_create_tasks.rb
@@ -4,7 +4,7 @@ class CreateTasks < ActiveRecord::Migration[5.1]
       t.string :name
       t.date :ending_date
       t.boolean :done
-      t.references :category, foreign_key: true
+      t.references :category, foreign_key: true, index: true
 
       t.timestamps
     end
