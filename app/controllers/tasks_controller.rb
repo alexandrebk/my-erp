3a380d340cacda81bd7a26b8b25f2ae2a07fbcd2
alexandrebk <alexandre.b2506@gmail.com>
2018-09-11 15:47:35 +0200

    initialisation simple form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,7 @@ class TasksController < ApplicationController
     skip_before_action :authenticate_user!, only: :index
 
   def index
+    @task = Task.new
   end
 
   def show
@@ -10,5 +11,17 @@ class TasksController < ApplicationController
 
   def finish
     @test = "test1"
+  end
+
+  def create
+    @task = Task.new
+    @task.save
+    redirect_to root_path
+  end
+
+private
+
+  def task_params
+    params.require(:article).permit(:name, :ending_date, :category_id)
   end
 end
